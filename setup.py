9e2719648789b07c6c934122974c31beb45dd5d2
Kuber Budhathoki <koobear99@gmail.com>
2024-12-19 12:17:45 +0545

    added author
    ama

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='censoredzz',
     version='1.0.0',
     description='Python package for censoring profane words in Nepali-(Roman) text',
-    author='Kuber Budhathoki',
-    author_email='koobear99@gmail.com',
+    author='Kuber Budhathoki, Amardeep Khatri',
+    author_email='koobear99@gmail.com, amrkhatri97@gmail.com',
     url='https://github.com/12-Twelvve/pypi_censored_text',
     packages=['censoredzz'],
     install_requires=[
